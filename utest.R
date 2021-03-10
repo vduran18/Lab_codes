fab83bf79565b41a4549beb5385630cf8007ec1b
Valeria Duran <57920512+vduran18@users.noreply.github.com>
2021-03-09 19:05:25 -0600

    Update utest.R

--- utest.R
+++ utest.R
@@ -1,6 +1,6 @@
 library(readxl)
 
-dat <- read_excel("/Users/valeriaduran/Downloads/BCa 2018 & 2019 Data med-N together cm Jun2020.xlsx", sheet = "Bridged Data cm")
+dat <- read_excel(x", sheet = "")
 
 # BC vs HC
 names(dat)
@@ -15,8 +15,6 @@ for(i in seq_along(1: nrow(dat1))){
 dat1$u_test_BCvsHC <- u_test_BCvsHC
 dat1 <- as.data.frame(dat1)
 names(dat1)
-write.csv2(dat1, "BC_utest_June19")
-remove.packages("data.table")
-install.packages("data.table")
+
 dat1 <- apply(dat1,2,as.character)
-write.csv(dat1, "BC_utest_June19.csv")
+write.csv(dat1, "BC")
